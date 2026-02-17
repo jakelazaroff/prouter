cd5f81ddd77f773cc9bb492a55bc2ed038ccc7ad
Jake Lazaroff <jake@phazein.com>
2026-02-17 16:07:53 -0500

    0.2.0

--- prouter.js
+++ prouter.js
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
-// prouter v0.1.0
+// prouter v0.2.0
 // https://github.com/jakelazaroff/prouter
 
 /** @import { AnyComponent, ComponentChildren, VNode } from "preact"; */
